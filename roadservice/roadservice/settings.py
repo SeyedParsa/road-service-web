3b6e7385923498bc210486b9df97224bfd2bdb5a
Mohammad Mahdi Shokri <mmahdishokri@gmail.com>
2021-06-19 21:12:27 +0430

    move the www dir to the correct place to be

--- roadservice/roadservice/settings.py
+++ roadservice/roadservice/settings.py
@@ -132,17 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'www', 'static'),
+    os.path.join(BASE_DIR,  'www', 'static'),
 ]
 
-STATIC_DIR = os.path.join(BASE_DIR, '..', 'www', 'static')
+STATIC_DIR = os.path.join(BASE_DIR,  'www', 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, '..','www','static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'www','static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'www', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,  'www', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
