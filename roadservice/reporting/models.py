c74d18a2d370e2dba78a764957431030f377871b
SeyedParsa <seyedparsamirtaheri@gmail.com>
2021-09-03 22:05:00 +0430

    apply changes due to review

--- roadservice/reporting/models.py
+++ roadservice/reporting/models.py
@@ -20,11 +20,11 @@ class IntervalReport(Report):
         self.start_time = start_time
         self.end_time = end_time
         self.interval_delta = timezone.timedelta(weeks=1)
-        self.bins = []
+        self.bin_starts = []
         self.team_counts = []
         self.issue_counts = []
         self.failed_issue_counts = []
-        self.done_issue_counts = []
+        self.successful_issue_counts = []
         self.score_averages = []
         self.Type = Report.Type.INTERVAL_REPORT
 
@@ -42,7 +42,8 @@ class DistributionReport(Report):
 class SubregionsReport(Report):
     def __init__(self, region):
         self.region = region
-        self.subregions_info = []
+        self.subregions_info = []  # the list would contain tuples of the form:
+        # (subregion, mission_count, issue_count, mission_score_avg, mission_success_rate, team_count)
         self.Type = Report.Type.SUBREGIONS_REPORT
 
 
@@ -60,27 +61,26 @@ class ReportGenerator:
             raise SingletonInitError()
         ReportGenerator.__instance = self
 
-    @staticmethod
-    def get_interval_report(region, start_time, end_time):
+    def get_interval_report(self, region, start_time, end_time):
         report = IntervalReport(region, start_time, end_time)
         time = start_time
         while time < end_time:
             bin_start = time
             bin_end = time + report.interval_delta
             time += report.interval_delta
-            report.bins.append(bin_start)
+            report.bin_starts.append(bin_start)
             report.team_counts.append(region.get_teams().filter(
                 Q(created_at__lt=bin_end) & (Q(deleted_at__gt=bin_start) | Q(deleted_at__isnull=True))).count())
             issues = region.get_issues().filter(created_at__gte=bin_start, created_at__lt=bin_end)
             report.issue_counts.append(issues.count())
             report.failed_issue_counts.append(issues.filter(state=Issue.State.FAILED).count())
-            report.done_issue_counts.append(issues.filter(state=Issue.State.DONE).count())
+            report.successful_issue_counts.append(issues.filter(
+                Q(state=Issue.State.DONE) | Q(state=Issue.State.SCORED)).count())
             missions = region.get_missions().filter(issue__created_at__gte=bin_start, issue__created_at__lt=bin_end)
             report.score_averages.append(missions.aggregate(Avg('score'))['score__avg'])
         return report
 
-    @staticmethod
-    def get_distribution_report(region):
+    def get_distribution_report(self, region):
         report = DistributionReport(region)
         for mission in region.get_missions():
             report.mission_type_dist[mission.type.name] += 1
@@ -92,12 +92,11 @@ class ReportGenerator:
             report.machinery_type_dist[machinery.type.name] += machinery.total_count
         return report
 
-    @staticmethod
-    def get_subregions_report(region):
+    def get_subregions_report(self, region):
         report = SubregionsReport(region)
-        for subregioin in region.sub_regions.all():
-            missions = subregioin.get_missions()
-            issues = subregioin.get_issues()
+        for subregion in region.sub_regions.all():
+            missions = subregion.get_missions()
+            issues = subregion.get_issues()
             mission_count = missions.count()
             issue_count = issues.count()
             mission_score_avg = missions.aggregate(Avg('score'))['score__avg']  # TODO
@@ -106,6 +105,7 @@ class ReportGenerator:
             finished_issues_count = successful_issues_count + failed_issues_count
             mission_success_rate = None if (finished_issues_count == 0) else \
                 successful_issues_count / finished_issues_count
-            report.subregions_info.append((subregioin, mission_count, issue_count,
-                                           mission_score_avg, mission_success_rate))
+            team_count = subregion.get_teams().count()
+            report.subregions_info.append((subregion, mission_count, issue_count,
+                                           mission_score_avg, mission_success_rate, team_count))
         return report
